f76f815b0c95733e24c11cdc3df0e3be700d122b
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2023-11-03 19:45:39 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -0,0 +1,40 @@
+class Solution {
+    static int[] discount = {10, 20, 30, 40};
+    static int maxOfSubscribe;
+    static int maxOfCost;
+
+    public int[] solution(int[][] users, int[] emoticons) {
+        DFS(0, emoticons.length, new int[emoticons.length], users, emoticons);
+        return new int[]{maxOfSubscribe, maxOfCost};
+    }
+
+    public void DFS(int L, int emoticonsLength, int[] discounts, int[][] users, int[] emoticons) {
+        if (L == emoticonsLength) {
+            int subscribe = 0;
+            int cost = 0;
+            for (int[] user : users) {
+                int userDiscountRate = user[0];
+                int userMaxCost = user[1];
+                int sum = 0;
+                for (int i = 0; i < emoticons.length; i++) {
+                    if (discounts[i] >= userDiscountRate) {
+                        sum += emoticons[i] / 100 * (100 - discounts[i]);
+                    }
+                }
+                if (sum >= userMaxCost) subscribe++;
+                else cost += sum;
+            }
+            if (subscribe > maxOfSubscribe) {
+                maxOfSubscribe = subscribe;
+                maxOfCost = cost;
+            } else if (subscribe == maxOfSubscribe) {
+                maxOfCost = Math.max(maxOfCost, cost);
+            }
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            discounts[L] = discount[i];
+            DFS(L + 1, emoticonsLength, discounts, users, emoticons);
+        }
+    }
+}
