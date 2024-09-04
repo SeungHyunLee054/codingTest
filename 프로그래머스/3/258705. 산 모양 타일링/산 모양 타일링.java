553b47e84bf1d547fc74d68dd5ec38b4d4bccf4b
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2024-09-04 11:35:46 +0900

    [level 3] Title: 산 모양 타일링, Time: 6.98 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -0,0 +1,22 @@
+class Solution {
+    private final int MOD = 10007;
+
+    public int solution(int n, int[] tops) {
+        int answer = 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            int top = tops[i];
+            int triangle = top == 0 ? 3 : 4;
+            int j = i + 1;
+            if (i == 0) {
+                dp[j] = triangle;
+            } else {
+                dp[j] = (triangle * dp[i] - dp[i - 1] + MOD) % MOD;
+            }
+        }
+        answer = dp[n] % MOD;
+
+        return answer;
+    }
+}
