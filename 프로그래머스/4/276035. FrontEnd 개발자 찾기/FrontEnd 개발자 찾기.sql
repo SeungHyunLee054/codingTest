9e988134ff1bb2782e902b6e0f979a0e65d2a0b0
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2024-09-06 10:49:41 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+select distinct d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+from DEVELOPERS d
+inner join SKILLCODES s
+on d.SKILL_CODE & s.CODE = s.CODE
+where s.CATEGORY = 'Front End'
+order by d.ID
+;
