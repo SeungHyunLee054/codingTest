07c26c8fed28456b36287a2cd8c447fa377f8399
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-02-28 19:45:20 +0900

    [level 1] Title: 최소직사각형, Time: 2.16 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,17 +2,17 @@ class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
         int maxVal = 0;
-        int minVal = 0;
+        int minMaxVal = 0;
 
         for (int[] size : sizes) {
             int bigVal = Integer.max(size[0], size[1]);
             int smallVal = Integer.min(size[0], size[1]);
 
             maxVal = Integer.max(maxVal, bigVal);
-            minVal = Integer.max(minVal, smallVal);
+            minMaxVal = Integer.max(minMaxVal, smallVal);
         }
 
-        answer = maxVal * minVal;
+        answer = maxVal * minMaxVal;
 
         return answer;
     }
