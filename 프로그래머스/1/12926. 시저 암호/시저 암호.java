7400d4e146734c94a4b1c2f3b333335ddbb71448
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-02-28 20:15:13 +0900

    [level 1] Title: 시저 암호, Time: 0.77 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append(' ');
+                continue;
+            }
+
+            int ascii = s.charAt(i);
+            int forwardAscii = ascii + n;
+
+            if (ascii <= 90 && forwardAscii > 90) {
+                forwardAscii -= 26;
+            }
+
+            if (forwardAscii > 122) {
+                forwardAscii -= 26;
+            }
+
+
+            sb.append((char) forwardAscii);
+        }
+
+        return sb.toString();
+    }
+}
