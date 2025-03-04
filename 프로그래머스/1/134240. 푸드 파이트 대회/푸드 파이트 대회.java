5ff30d9de80772aa6b6b9652db48c02c5265e3df
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-04 10:00:26 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.25 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -11,12 +11,17 @@ class Solution {
         }
 
         for (int i = 1; i < list.size(); i++) {
-            sb.append(String.valueOf(i).repeat(Math.max(0, list.get(i))));
+            sb.append(String.valueOf(i).repeat(list.get(i)));
         }
 
         String reverse = sb.reverse().toString();
         sb.reverse().append(0).append(reverse);
 
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[]{1, 3, 4, 6}));
     }
 }
