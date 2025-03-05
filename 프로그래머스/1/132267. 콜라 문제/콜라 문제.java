9603a2caae5b4312ede856012df0f97e701cc0f4
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-05 09:13:27 +0900

    [level 1] Title: 콜라 문제, Time: 2.22 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,13 +2,13 @@ class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
     
-        int Fbottle = b;
+        int fBottle = b;
 
         if(b >= 1 && a > b && n >=a && n <= 1000000){
             while(n >= a){
-                n = (n - a + Fbottle);
-                Fbottle = b; 
-                answer += Fbottle;
+                n = (n - a + fBottle);
+                fBottle = b; 
+                answer += fBottle;
             }
         }
         else{
