03f3071859aef1926aafc076062e90cc91b70fa8
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-05 09:40:47 +0900

    [level 1] Title: 모의고사, Time: 5.23 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,49 +1,39 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     public int[] solution(int[] answers) {
-        List<Integer> student1 = new ArrayList<>();
-        List<Integer> student2 = new ArrayList<>();
-        List<Integer> student3 = List.of(3, 3, 1, 1, 2, 2, 4, 4, 5, 5);
-        for (int i = 1; i < 6; i++) {
-            student1.add(i);
-            if (i != 2) {
-                student2.add(2);
-                student2.add(i);
-            }
-        }
+        List<Integer> firstStudent = List.of(1, 2, 3, 4, 5);
+        List<Integer> secondStudent = List.of(2, 1, 2, 3, 2, 4, 2, 5);
+        List<Integer> thirdStudent = List.of(3, 3, 1, 1, 2, 2, 4, 4, 5, 5);
 
-        int[] result = new int[3];
+        int[] score = new int[3];
         for (int i = 0; i < answers.length; i++) {
             int ans = answers[i];
 
-            if (ans == student1.get(i % 5)) {
-                result[0]++;
+            if (ans == firstStudent.get(i % 5)) {
+                score[0]++;
             }
-            if (ans == student2.get(i % 8)) {
-                result[1]++;
+            if (ans == secondStudent.get(i % 8)) {
+                score[1]++;
             }
-            if (ans == student3.get(i % 10)) {
-                result[2]++;
+            if (ans == thirdStudent.get(i % 10)) {
+                score[2]++;
             }
         }
 
-        if (result[0] > result[1] && result[0] > result[2]) {
-            return new int[]{1};
-        } else if (result[1] > result[0] && result[1] > result[2]) {
-            return new int[]{2};
-        } else if (result[2] > result[0] && result[2] > result[1]) {
-            return new int[]{3};
-        } else if (result[0] == result[1] && result[0] > result[2]) {
-            return new int[]{1, 2};
-        } else if (result[0] == result[2] && result[0] > result[1]) {
-            return new int[]{1, 3};
-        } else if (result[1] > result[0]) {
-            return new int[]{2, 3};
-        } else {
-            return new int[]{1, 2, 3};
+        int maxScore = Arrays.stream(score).max().getAsInt();
+
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            if (maxScore == score[i]) {
+                answer.add(i + 1);
+            }
         }
 
+        return answer.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
