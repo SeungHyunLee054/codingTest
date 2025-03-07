7df4737096ee1749e5ad4f9c7ce562401f77c15b
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-07 09:23:27 +0900

    [level 1] Title: 소수 만들기, Time: 4.88 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -5,14 +5,16 @@ class Solution {
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    boolean flg = false;
                     int temp = nums[i] + nums[j] + nums[k];
+
+                    boolean flg = false;
                     for (int l = 2; l <= Math.sqrt(temp); l++) {
                         if (temp % l == 0) {
                             flg = true;
                             break;
                         }
                     }
+                    
                     if (!flg) {
                         answer++;
                     }
