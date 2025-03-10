8649eee5a13c7cc679d20821095aa69b639def67
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-10 10:08:09 +0900

    [level 1] Title: 옹알이 (2), Time: 3.43 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,29 +3,24 @@ class Solution {
         String[] sayingWords = new String[]{"aya", "ye", "woo", "ma"};
         int answer = 0;
 
-        for (int i = 0; i < babbling.length; i++) {
-            if (babbling[i].contains(sayingWords[0] + sayingWords[0]) ||
-                    babbling[i].contains(sayingWords[1] + sayingWords[1]) ||
-                    babbling[i].contains(sayingWords[2] + sayingWords[2]) ||
-                    babbling[i].contains(sayingWords[3] + sayingWords[3])) {
+        for (String babblingWord : babbling) {
+            if (babblingWord.contains(sayingWords[0] + sayingWords[0]) ||
+                    babblingWord.contains(sayingWords[1] + sayingWords[1]) ||
+                    babblingWord.contains(sayingWords[2] + sayingWords[2]) ||
+                    babblingWord.contains(sayingWords[3] + sayingWords[3])) {
                 continue;
             }
 
-            babbling[i] = babbling[i].replace(sayingWords[0], " ");
-            babbling[i] = babbling[i].replace(sayingWords[1], " ");
-            babbling[i] = babbling[i].replace(sayingWords[2], " ");
-            babbling[i] = babbling[i].replace(sayingWords[3], " ");
-            babbling[i] = babbling[i].replace(" ", "");
+            babblingWord = babblingWord.replace(sayingWords[1], " ");
+            babblingWord = babblingWord.replace(sayingWords[2], " ");
+            babblingWord = babblingWord.replace(sayingWords[3], " ");
+            babblingWord = babblingWord.replace(sayingWords[0], " ");
+            babblingWord = babblingWord.replace(" ", "");
 
-            if (babbling[i].isEmpty()) {
+            if (babblingWord.isEmpty()) {
                 answer++;
             }
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.solution(new String[]{"aya", "yee", "u", "maa"}));
     }
 }
