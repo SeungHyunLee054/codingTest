b887bf1f4fb8e380371fcb15a705c1189f708ee8
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-13 09:53:50 +0900

    [level 1] Title: 둘만의 암호, Time: 0.91 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            int ascii = s.charAt(i);
+
+            int j = 0;
+            int repeat = index;
+            while (j < repeat) {
+                int nextAscii = ascii > 121 ? ascii - 25 : ascii + 1;
+                for (int k = 0; k < skip.length(); k++) {
+                    if (nextAscii == skip.charAt(k)) {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    repeat += 1;
+                    flag = false;
+                }
+
+                ascii = nextAscii;
+                j++;
+            }
+
+            if (ascii > 122) {
+                ascii -= 26;
+            }
+
+
+            sb.append((char) ascii);
+        }
+
+        return sb.toString();
+    }
+}
