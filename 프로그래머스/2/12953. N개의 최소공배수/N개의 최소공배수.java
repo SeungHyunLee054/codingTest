f411c98a2083ceb779aea1d0865b5a271f32e16b
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-21 09:49:25 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] arr) {
+        return getLCM(arr);
+    }
+
+    private int getGCD(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return getGCD(b, a % b);
+    }
+
+    private int getLCM(int[] arr) {
+        if (arr.length < 2) {
+            return arr[0];
+        }
+
+        int gcd = getGCD(arr[0], arr[1]);
+        int lcm = (arr[0] * arr[1]) / gcd;
+
+        for (int i = 2; i < arr.length; i++) {
+            gcd = getGCD(lcm, arr[i]);
+            lcm = (lcm * arr[i]) / gcd;
+        }
+
+        return lcm;
+    }
+}
