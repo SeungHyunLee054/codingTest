62fb9936363a1c63e27503a92443d6024c481add
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-21 09:41:24 +0900

    [level 2] Title: 예상 대진표, Time: 0.28 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+
+        while (a != b) {
+            a = a % 2 == 0 ? a / 2 : (a + 1) / 2;
+            b = b % 2 == 0 ? b / 2 : (b + 1) / 2;
+            answer++;
+        }
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println(answer);
+
+        return answer;
+    }
+}
