8524feb3654a743f964fa679e06a1c14f5adabab
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-25 09:51:40 +0900

    [level 2] Title: 괄호 회전하기, Time: 233.04 ms, Memory: 155 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        Stack<String> stack = new Stack<>();
+        boolean flag = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            String sub = s.substring(i) + s.substring(0, i);
+
+            for (String subSp : sub.split("")) {
+                if (subSp.equals("(") || subSp.equals("{") || subSp.equals("[")) {
+                    stack.push(subSp);
+                } else if (stack.isEmpty()) {
+                    flag = false;
+                    break;
+                } else {
+                    if (stack.peek().equals("(") && subSp.equals(")")) {
+                        stack.pop();
+                    } else if (stack.peek().equals("{") && subSp.equals("}")) {
+                        stack.pop();
+                    } else if (stack.peek().equals("[") && subSp.equals("]")) {
+                        stack.pop();
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            if (flag && stack.isEmpty()) {
+                answer++;
+            }
+
+            flag = true;
+            stack.clear();
+        }
+
+        return answer;
+    }
+}
