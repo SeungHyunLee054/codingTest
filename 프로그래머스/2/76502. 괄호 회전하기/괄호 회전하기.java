50b03c8b23fa0888552fcf14c1df158f3f31faff
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-25 09:52:23 +0900

    [level 2] Title: 괄호 회전하기, Time: 241.19 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -40,9 +40,4 @@ class Solution {
 
         return answer;
     }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.solution("[](){}"));
-    }
 }
