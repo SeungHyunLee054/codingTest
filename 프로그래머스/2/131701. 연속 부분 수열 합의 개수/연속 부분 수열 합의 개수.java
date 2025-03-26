77c21d58f6af95398bd6cc90c90a4de6e7af3eb3
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-26 09:46:28 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 104.39 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,19 +1,18 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
     public int solution(int[] elements) {
-        int[] newElements = new int[elements.length * 2];
-        for (int i = 0; i < elements.length; i++) {
-            newElements[i] = elements[i];
-            newElements[i + elements.length] = elements[i];
+        int n = elements.length;
+        int[] prefixSum = new int[2 * n + 1];
+        for (int i = 1; i <= 2 * n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + elements[(i - 1) % n];
         }
 
         Set<Integer> set = new HashSet<>();
         for (int i = 1; i <= elements.length; i++) {
             for (int j = 0; j < elements.length; j++) {
-                set.add(Arrays.stream(newElements, j, j + i).sum());
+                set.add(prefixSum[i + j] - prefixSum[j]);
             }
         }
 
