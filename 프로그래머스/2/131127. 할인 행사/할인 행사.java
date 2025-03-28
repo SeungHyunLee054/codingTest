2c3114e4972f68f4f7c760d23c04584918468c8d
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-28 09:53:31 +0900

    [level 2] Title: 할인 행사, Time: 72.94 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -8,27 +8,24 @@ class Solution {
         Map<String, Integer> map = new HashMap<>();
         boolean flag = true;
 
-        for (int i = 0; i < discount.length; i++) {
-            if (i + 10 <= discount.length) {
-                for (int j = i; j < i + 10; j++) {
-                    map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
-                }
-
-                for (int j = 0; j < want.length; j++) {
-                    if (map.get(want[j]) == null || map.get(want[j]) < number[j]) {
-                        flag = false;
-                        break;
-                    }
-                }
-
-                if (flag) {
-                    answer++;
-                } else {
-                    flag = true;
-                }
-
-                map.clear();
+        for (int i = 0; i <= Math.max(0, discount.length - 10); i++) {
+            for (int j = i; j < i + 10; j++) {
+                map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
             }
+
+            for (int j = 0; j < want.length; j++) {
+                if (map.get(want[j]) == null || map.get(want[j]) < number[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                answer++;
+            }
+
+            flag = true;
+            map.clear();
         }
 
         return answer;
