5eb2ec91145dfd2336ef35b8219eb529da3b36fa
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-28 09:49:19 +0900

    [level 2] Title: 할인 행사, Time: 91.25 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+
+        Map<String, Integer> map = new HashMap<>();
+        boolean flag = true;
+
+        for (int i = 0; i < discount.length; i++) {
+            if (i + 10 <= discount.length) {
+                for (int j = i; j < i + 10; j++) {
+                    map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
+                }
+
+                for (int j = 0; j < want.length; j++) {
+                    if (map.get(want[j]) == null || map.get(want[j]) < number[j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    answer++;
+                } else {
+                    flag = true;
+                }
+
+                map.clear();
+            }
+        }
+
+        return answer;
+    }
+}
