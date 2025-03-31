39a1131f8bbc222ee12556ec9b90e696bcbe71ec
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-31 09:18:19 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,16 +1,18 @@
 import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-        public int solution(String[][] clothes) {
-            HashMap<String, Integer> hm = new HashMap<>();
-            for (int i = 0; i < clothes.length; i++) {
-                hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 1) + 1);
-            }
-
-            int answer = 1;
-            for (String key : hm.keySet()) {
-                answer *= hm.get(key);
-            }
-            return answer - 1;
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 1) + 1);
         }
+
+        int answer = 1;
+        for (String key : map.keySet()) {
+            answer *= map.get(key);
+        }
+        
+        return answer - 1;
     }
+}
