9fc4659ecae69c0d691fd950de32c78f0ee541be
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-03-31 09:24:31 +0900

    [level 2] Title: 프로세스, Time: 1.74 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
 class Solution {
-        public int solution(int[] priorities, int location) {
-            int answer = 0;
-            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
 
+        for (int priority : priorities) {
+            priorityQueue.offer(priority);
+        }
+
+        while (!priorityQueue.isEmpty()) {
             for (int i = 0; i < priorities.length; i++) {
-                priorityQueue.offer(priorities[i]);
-            }
-
-            while (!priorityQueue.isEmpty()) {
-                for (int i = 0; i < priorities.length; i++) {
-                    if (priorityQueue.peek() == priorities[i]) {
-                        priorityQueue.poll();
-                        answer++;
-                        if (i == location) {
-                            return answer;
-                        }
+                if (priorityQueue.peek() == priorities[i]) {
+                    priorityQueue.poll();
+                    answer++;
+                    if (i == location) {
+                        return answer;
                     }
                 }
             }
-            
-            return answer;
         }
+
+        return answer;
     }
+}
