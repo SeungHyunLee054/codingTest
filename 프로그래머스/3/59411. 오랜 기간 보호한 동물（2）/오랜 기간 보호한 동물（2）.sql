45f25f41be91353a632f4d75ab545187fa6f9bcb
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-04-03 09:11:04 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT io.ANIMAL_ID, io.NAME
+from (
+    select o.ANIMAL_ID, o.NAME, o.DATETIME-i.DATETIME
+from ANIMAL_OUTS o
+join ANIMAL_INS i
+on i.ANIMAL_ID = o.ANIMAL_ID
+order by o.DATETIME-i.DATETIME desc
+limit 2
+) io
+;
