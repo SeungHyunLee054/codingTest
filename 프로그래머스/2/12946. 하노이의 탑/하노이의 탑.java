63e450e833f8d871238395ecac3020a85d668ded
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-05-09 10:11:26 +0900

    [level 2] Title: 하노이의 탑, Time: 3.80 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -2,21 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
+	List<int[]> list = new ArrayList<>();
+
 	public int[][] solution(int n) {
-		List<int[]> list = new ArrayList<>();
-		dfs(n, 1, 2, 3, list);
+		dfs(n, 1, 2, 3);
 
 		return list.toArray(int[][]::new);
 	}
 
-	private void dfs(int n, int from, int by, int to, List<int[]> list) {
+	private void dfs(int n, int from, int by, int to) {
 		if (n == 1) {
 			list.add(new int[] {from, to});
 			return;
 		}
 
-		dfs(n - 1, from, to, by, list);
+		dfs(n - 1, from, to, by);
 		list.add(new int[] {from, to});
-		dfs(n - 1, by, from, to, list);
+		dfs(n - 1, by, from, to);
 	}
 }
