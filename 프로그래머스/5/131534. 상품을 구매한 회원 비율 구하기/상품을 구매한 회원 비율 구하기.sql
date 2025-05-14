cf12c6d1d6794ec5a1bc51ad24e2fa8bf75bc5fa
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-05-14 09:31:05 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,9 +1,12 @@
 -- 코드를 입력하세요
-SELECT year(o.SALES_DATE) as YEAR, month(o.SALES_DATE) as MONTH, count(distinct u.USER_ID) as PURCHASED_USERS,
-round(count(distinct u.USER_ID) / (select count(*) from USER_INFO where year(JOINED) = '2021'), 1) as PURCHASED_RATIO
+SELECT year(o.SALES_DATE)                                                                                 as YEAR,
+       month(o.SALES_DATE)                                                                                as MONTH,
+       count(distinct u.USER_ID)                                                                          as PURCHASED_USERS,
+       round(count(distinct u.USER_ID) / (select count(*) from USER_INFO where year(JOINED) = '2021'),
+             1)                                                                                           as PURCHASED_RATIO
 from USER_INFO u
-join ONLINE_SALE o
-on u.USER_ID = o.USER_ID
+         join ONLINE_SALE o
+              on u.USER_ID = o.USER_ID
 where year(u.JOINED) = '2021'
 group by YEAR, MONTH
 order by YEAR, MONTH
