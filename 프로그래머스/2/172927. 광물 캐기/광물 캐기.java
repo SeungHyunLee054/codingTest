ca7932a3b20ac2370de3c75c5ec965d641195c4a
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-05-14 11:20:02 +0900

    [level 2] Title: 광물 캐기, Time: 3.17 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -10,34 +10,49 @@ class Solution {
 
 		int pickSum = Arrays.stream(picks).sum() * 5;
 
-		List<int[]> mineralList = new ArrayList<>();
+		List<Mine> mineralList = new ArrayList<>();
 		for (int i = 0; i < pickSum && i < minerals.length; i += 5) {
-			int[] block = new int[3];
+			Mine mine = new Mine(0, 0, 0);
 			for (int j = i; j < i + 5 && j < pickSum && j < minerals.length; j++) {
 				switch (minerals[j]) {
-					case "diamond" -> block[0]++;
-					case "iron" -> block[1]++;
-					case "stone" -> block[2]++;
+					case "diamond" -> mine.diamond++;
+					case "iron" -> mine.iron++;
+					case "stone" -> mine.stone++;
 				}
 			}
-			mineralList.add(block);
+			mineralList.add(mine);
 		}
 
-		mineralList.sort((a, b) -> {
-			int aTired = a[0] * pickax[2][0] + a[1] * pickax[2][1] + a[2];
-			int bTired = b[0] * pickax[2][0] + b[1] * pickax[2][1] + b[2];
-			return bTired - aTired;
-		});
+		mineralList.sort(Mine::compareTo);
 
 		int idx = 0;
 		for (int i = 0; i < 3; i++) {
 			while (picks[i] > 0 && idx < mineralList.size()) {
-				int[] cur = mineralList.get(idx++);
-				answer += cur[0] * pickax[i][0] + cur[1] * pickax[i][1] + cur[2] * pickax[i][2];
+				Mine mine = mineralList.get(idx++);
+				answer += mine.diamond * pickax[i][0] + mine.iron * pickax[i][1] + mine.stone * pickax[i][2];
 				picks[i]--;
 			}
 		}
 
 		return answer;
+	}
+
+	class Mine implements Comparable<Mine> {
+		int diamond;
+		int iron;
+		int stone;
+
+		public Mine(int diamond, int iron, int stone) {
+			this.diamond = diamond;
+			this.iron = iron;
+			this.stone = stone;
+		}
+
+		@Override
+		public int compareTo(Mine o) {
+			int a = this.diamond * 25 + this.iron * 5 + this.stone;
+			int b = o.diamond * 25 + o.iron * 5 + o.stone;
+			return b - a;
+		}
 	}
 }
