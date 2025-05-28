56d25ea81215c01e670759a00089bc7bc12fc9d0
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-05-28 09:42:55 +0900

    Time: 730 ms (28.1%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
+++ LeetCode/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,14 +1,6 @@
 # Write your MySQL query statement below
-with confirmed as
-         (select user_id, sum(action = 'confirmed') as confirmed_count, count(*) as total_count
-          from Confirmations
-          group by user_id)
-
-select s.user_id,
-       case
-           when c.total_count is null or c.total_count = 0 then 0
-           else round(ifnull(c.confirmed_count, 0) / ifnull(c.total_count, 0), 2)
-           end as confirmation_rate
+select s.user_id, round(ifnull(avg(action = 'confirmed'), 0), 2) as confirmation_rate
 from Signups s
-left join confirmed c on s.user_id = c.user_id
+left join Confirmations c on s.user_id = c.user_id
+group by user_id
 ;
