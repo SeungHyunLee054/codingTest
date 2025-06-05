969fcc9859393b2938245edaff78710181a751e6
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-06-05 10:15:14 +0900

    [level 3] Title: 이중우선순위큐, Time: 60.48 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,33 +1,28 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.TreeSet;
 
 class Solution {
 	public int[] solution(String[] operations) {
-		PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
-		PriorityQueue<Integer> min = new PriorityQueue<>();
+		TreeSet<Integer> set = new TreeSet<>();
 		for (String operation : operations) {
 			String[] split = operation.split(" ");
 			String command = split[0];
 			String num = split[1];
 
 			if (command.equals("I")) {
-				max.offer(Integer.parseInt(num));
-				min.offer(Integer.parseInt(num));
+				set.add(Integer.parseInt(num));
 			} else if (command.equals("D")) {
-				if (max.isEmpty()) {
+				if (set.isEmpty()) {
 					continue;
 				}
 
 				if (num.charAt(0) == '-') {
-					Integer minVal = min.poll();
-					max.remove(minVal);
+					set.pollFirst();
 				} else {
-					Integer maxVal = max.poll();
-					min.remove(maxVal);
+					set.pollLast();
 				}
 			}
 		}
 
-		return max.isEmpty() && min.isEmpty() ? new int[] {0, 0} : new int[] {max.peek(), min.peek()};
+		return set.isEmpty() ? new int[] {0, 0} : new int[] {set.last(), set.first()};
 	}
 }
