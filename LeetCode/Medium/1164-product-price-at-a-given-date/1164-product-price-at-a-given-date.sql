06830082bebf14038044a42fb468b77b8b0a55c8
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-06-17 10:01:44 +0900

    Time: 1471 ms (5.4%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Medium/1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select p.product_id, coalesce(
+    (select new_price from Products where product_id = p.product_id and change_date <= '2019-08-16' order by change_date desc limit 1), 10
+) as price
+from Products p
+group by product_id
+;
