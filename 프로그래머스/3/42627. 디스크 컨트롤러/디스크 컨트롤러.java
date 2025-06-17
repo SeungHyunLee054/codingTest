a702a086d469063d74bd548d192642b6233810ff
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-06-17 10:19:15 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 6.38 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,32 +3,32 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Solution {
-        public int solution(int[][] jobs) {
-            int answer = 0;
-            int idx=0;
-            int cnt=0;
-            int total=0;
-            int end=0;
+	public int solution(int[][] jobs) {
+		int answer = 0;
+		int idx = 0;
+		int cnt = 0;
+		int total = 0;
+		int end = 0;
 
-            Arrays.sort(jobs, Comparator.comparingInt(a -> a[0]));
-            PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+		Arrays.sort(jobs, Comparator.comparingInt(a -> a[0]));
+		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
 
-            while (cnt < jobs.length) {
-                while (idx< jobs.length&&jobs[idx][0]<=end){
-                    pq.add(jobs[idx++]);
-                }
-                
-                if (pq.isEmpty()){
-                    end=jobs[idx][0];
-                }else {
-                    int[] cur=pq.poll();
-                    total+=cur[1]+end-cur[0];
-                    end+=cur[1];
-                    cnt++;
-                }
-            }
-            
-            answer=total/ jobs.length;
-            return answer;
-        }
-    }
+		while (cnt < jobs.length) {
+			while (idx < jobs.length && jobs[idx][0] <= end) {
+				pq.add(jobs[idx++]);
+			}
+
+			if (pq.isEmpty()) {
+				end = jobs[idx][0];
+			} else {
+				int[] cur = pq.poll();
+				total += cur[1] + end - cur[0];
+				end += cur[1];
+				cnt++;
+			}
+		}
+
+		answer = total / jobs.length;
+		return answer;
+	}
+}
