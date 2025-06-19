3cc3c9a907c11d80e768471c9c3df7d766270aa9
SeungHyunLee054 <103303970+SeungHyunLee054@users.noreply.github.com>
2025-06-19 10:01:07 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 17.23 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -5,8 +5,6 @@ class Solution {
 		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 		System.out.println("Hello Java");
 
-		int n = s.length();
-
 		for (int i = 0; i < s.length(); i++) {
 			int odd = expand(i, i, s);
 			int even = expand(i, i + 1, s);
